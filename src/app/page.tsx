bf03be8173dfc89bad7c7c27f512fb4acc99d187
unknown <atikrahad1@gmail.com>
2024-03-24 14:54:40 +0600

    adding footer for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/ui/Landingpage/Banner";
+import Footer from "@/components/ui/Landingpage/Footer";
 import Navber from "@/components/ui/Landingpage/Navber";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="h-[1000px]">
       <Navber/>
       <Banner/>
+      <Footer/>
     </div>
   );
 }
